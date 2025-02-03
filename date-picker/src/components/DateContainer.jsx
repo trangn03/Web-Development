0f1a0557378b29f144a21c662fa0038ea0070e6c
trangn03 <trangn0102@csu.fullerton.edu>
2025-02-02 20:49:49 -0800

    React js

--- date-picker/src/components/DateContainer.jsx
+++ date-picker/src/components/DateContainer.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+import "./date.css";
+import DateDisplay from "./DateDisplay";
+
 const DateContainer = () => {
+    const [date, setDate] = useState(Date.now()); //useState
+    useEffect(()=>{
+        
+    }, [date])
+
     return ( 
         <div className="date-container">
-            Hello there.
+            <input type="date" className="date-input" onChange={(e)=> setDate(e.target.value)}/>
+            <DateDisplay date={date}/>
         </div>    
      );
 }
