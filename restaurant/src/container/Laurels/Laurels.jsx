f18696c61dafa6122689a978bcaf88868def8bf6
trangn03 <xtrang0201@gmail.com>
2025-02-26 23:04:50 -0800

    Implemented Navbar

---
+++ restaurant/src/container/Laurels/Laurels.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Laurels = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Laurels
---
+++ restaurant/src/container/Menu/SpecialMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpecialMenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SpecialMenu
