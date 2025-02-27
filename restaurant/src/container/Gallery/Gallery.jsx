5d72a04b2c8aa34de46df60c7eb87ea7776340b3
trangn03 <xtrang0201@gmail.com>
2025-02-27 14:45:20 -0800

    Implement Award and recognition portions

--- restaurant/src/container/Gallery/Gallery.jsx
+++ restaurant/src/container/Gallery/Gallery.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import './Gallery.css'
+import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from "react-icons/bs";
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 const Gallery = () => {
+
+
+
   return (
     <div>
       
